1f795e8a13bdf21e2ab560d70340eac91835773d
jamesmaddox-cf <98417364+jamesmaddox-cf@users.noreply.github.com>
2023-07-31 16:26:12 -0400

    Adding checkov skip comment for KMS

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ resource "google_kms_crypto_key" "key" {
 
   key_ring        = google_kms_key_ring.key_ring.id
   name            = var.keys[count.index]
+  #checkov:skip=CKV_GCP_43:Rotation period is correctly set for daily
   rotation_period = var.key_rotation_period
   purpose         = var.purpose
 
