73047720d6509696d4397ca2a13094c0a3721f22
Matthew Pappas <117243210+matt-pappas-cf@users.noreply.github.com>
2024-03-04 13:40:23 -0800

    pub key output

--- modules/key/outputs.tf
+++ modules/key/outputs.tf
@@ -12,3 +12,7 @@ output "version_name" {
   description = "KMS key version name."
   value       = data.google_kms_crypto_key_version.version.name
 }
+
+output "kms_public_key_value" {
+  value = try(data.google_kms_crypto_key_version.version.public_key, "Error: Only Asymmetric keys have public key value")
+}
